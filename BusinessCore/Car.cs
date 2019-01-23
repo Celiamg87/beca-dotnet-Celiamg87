500af2d3c39941fdf63758863e34268171b7e199
Celiamg <celiamargil@gmail.com>
2019-01-23 09:13:51 +0100

    Add class Car

---
+++ BusinessCore/Car.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessCore
+{
+    class Car : Vehicle
+    {
+        public Car() : base()
+        {
+        }
+
+        public Car(int door, int wheel, string chasis, bool extras, string carroceria) : base(door, wheel, chasis, extras, carroceria)
+        {
+        }
+
+        public void montarCoche(int door, int wheel, string chasis, bool extras, string carroceria)
+        {
+            this.setDoor(door);
+            this.setWheel(wheel);
+            this.setCahsis(chasis);
+            this.setExtras(extras);
+            this.setCarroceria(carroceria);
+        }
+    }
+}
