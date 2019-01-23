8cb02daa10981bb5c88411107e30b82d57f641a9
Celiamg <celiamargil@gmail.com>
2019-01-23 09:13:34 +0100

    Add class Motocycle

---
+++ BusinessCore/motorcycle.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessCore
+{
+    class Motorcycle : Vehicle
+    {
+        public Motorcycle() : base(0, 2, "bastidor", false, "red")
+        {
+        }
+
+        public Motorcycle(int wheel, string chasis, bool extras, string carroceria) : base(0, wheel, chasis, extras, carroceria)
+        {
+        }
+
+        public void montarMoto(int wheel, string chasis, bool extras, string carroceria)
+        {
+            this.setWheel(wheel);
+            this.setCahsis(chasis);
+            this.setExtras(extras);
+            this.setCarroceria(carroceria);
+        }
+
+    }
+}
